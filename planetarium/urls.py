73033bf804c5f4504cae817b8822c4d93a13626f
Andriy Hrudnyy <andgrudniy@gmail.com>
2025-05-15 21:52:29 +0200

    Added views and urls

--- planetarium/urls.py
+++ planetarium/urls.py
@@ -1,13 +1,22 @@
 from django.urls import path, include
 from rest_framework import routers
 
+from planetarium.views import (
+    ShowThemeView,
+    AstronomyShowView,
+    PlanetariumDomeView,
+    ShowSessionView,
+    ReservationView,
+    TicketView
+)
+
 router = routers.DefaultRouter()
-#router.register("genres", GenreViewSet)
-#router.register("actors", ActorViewSet)
-#router.register("cinema_halls", CinemaHallViewSet)
-#router.register("movies", MovieViewSet)
-#router.register("movie_sessions", MovieSessionViewSet)
-#router.register("orders", OrderViewSet)
+router.register("themes", ShowThemeView)
+router.register("astronomy_shows", AstronomyShowView)
+router.register("planetarium_domes", PlanetariumDomeView)
+router.register("sessions", ShowSessionView)
+router.register("reservations", ReservationView)
+router.register("tickets", TicketView)
 
 urlpatterns = [path("", include(router.urls))]
 
--- planetarium/views.py
+++ planetarium/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from planetarium.models import (
+    ShowTheme,
+    AstronomyShow,
+    PlanetariumDome,
+    ShowSession,
+    Reservation,
+    Ticket,
+)
+
+from planetarium.serializers import (
+    ShowThemeSerializer,
+    AstronomyShowSerializer,
+    PlanetariumDomeSerializer,
+    ShowSessionSerializer,
+    ReservationSerializer,
+    TicketSerializer,
+)
+
+
+class ShowThemeView(viewsets.ModelViewSet):
+    queryset = ShowTheme.objects.all()
+    serializer_class = ShowThemeSerializer
+
+
+class AstronomyShowView(viewsets.ModelViewSet):
+    queryset = AstronomyShow.objects.all()
+    serializer_class = AstronomyShowSerializer
+
+
+class PlanetariumDomeView(viewsets.ModelViewSet):
+    queryset = PlanetariumDome.objects.all()
+    serializer_class = PlanetariumDomeSerializer
+
+
+class ShowSessionView(viewsets.ModelViewSet):
+    queryset = ShowSession.objects.all()
+    serializer_class = ShowSessionSerializer
+
+
+class ReservationView(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+
+
+class TicketView(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
