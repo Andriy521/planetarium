eb47f19f791f4f130902a3ec33de3f4b918dc23c
Andriy Hrudnyy <andgrudniy@gmail.com>
2025-05-20 19:48:23 +0200

    Added requirements.txt and migrated project to Docker

--- planetarium_service/settings.py
+++ planetarium_service/settings.py
@@ -11,21 +11,24 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR  / '.env')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$t1bahlmj95k2pdkq0177648!sdex)a_d-zb61(uspt7*7fu(8'
+SECRET_KEY = os.environ.get("SECRET_KEY", "insecure-dev-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split()
 
 
 # Application definition
@@ -78,8 +81,12 @@ WSGI_APPLICATION = 'planetarium_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("DB_NAME", "planetarium_db"),
+        'USER': os.environ.get("DB_USER", "planetarium_user"),
+        'PASSWORD': os.environ.get("DB_PASSWORD", "planetarium_pass"),
+        'HOST': os.environ.get("DB_HOST", "localhost"),
+        'PORT': os.environ.get("DB_PORT", "5432"),
     }
 }
 
